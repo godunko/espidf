489db2f0e2d566d8135522634951408be96ae7d8
Vadim Godunko <vgodunko@gmail.com>
2026-04-19 08:14:05 +0400

    Initial.

---
+++ source/espidf-driver.ads
@@ -0,0 +1,12 @@
+--
+--  Copyright (C) 2026, Vadim Godunko
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+--
+
+package ESPIDF.Driver with Pure is
+
+   type gpio_num_t is new int;
+   --  XXX Move to GPIO driver
+
+end ESPIDF.Driver;
