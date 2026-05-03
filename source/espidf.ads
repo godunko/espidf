4f29adc52d2e73cb4ad990ef2dee71e4923f8068
Vadim Godunko <vgodunko@gmail.com>
2026-05-03 10:46:14 +0400

    Add common ESP-IDF error codes.

--- source/espidf.ads
+++ source/espidf.ads
@@ -19,7 +19,21 @@ package ESPIDF with Pure is
    type esp_err_t is new int;
 
    ESP_OK                   : constant esp_err_t := 0;
+   ESP_FAIL                 : constant esp_err_t := -1;
+
+   ESP_ERR_NO_MEM           : constant esp_err_t := 16#101#;
+   ESP_ERR_INVALID_ARG      : constant esp_err_t := 16#102#;
+   ESP_ERR_INVALID_STATE    : constant esp_err_t := 16#103#;
+   ESP_ERR_INVALID_SIZE     : constant esp_err_t := 16#104#;
+   ESP_ERR_NOT_FOUND        : constant esp_err_t := 16#105#;
+   ESP_ERR_NOT_SUPPORTED    : constant esp_err_t := 16#106#;
+   ESP_ERR_TIMEOUT          : constant esp_err_t := 16#107#;
    ESP_ERR_INVALID_RESPONSE : constant esp_err_t := 16#108#;
+   ESP_ERR_INVALID_CRC      : constant esp_err_t := 16#109#;
+   ESP_ERR_INVALID_VERSION  : constant esp_err_t := 16#10A#;
+   ESP_ERR_INVALID_MAC      : constant esp_err_t := 16#10B#;
+   ESP_ERR_NOT_FINISHED     : constant esp_err_t := 16#10C#;
+   ESP_ERR_NOT_ALLOWED      : constant esp_err_t := 16#10D#;
 
    function nul return char is (Interfaces.C.nul);
 
