ee65ea91bf220865d6e529bbc4bcf6e357330478
Vadim Godunko <vgodunko@gmail.com>
2026-09-14 19:57:51 +0400

    Use type `int`

--- source/espidf.ads
+++ source/espidf.ads
@@ -17,7 +17,7 @@ package ESPIDF with Pure is
 
    subtype bool     is Interfaces.C.C_bool;
    subtype char     is Interfaces.C.char;      use type char;
-   subtype int      is Interfaces.C.int;
+   subtype int      is Interfaces.C.int;       use type int;
    subtype int32_t  is Interfaces.Integer_32;  use type int32_t;
    subtype uint8_t  is Interfaces.Unsigned_8;  use type uint8_t;
    subtype uint16_t is Interfaces.Unsigned_16;
